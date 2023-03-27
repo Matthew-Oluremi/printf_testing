53b95e3648017965cec15e5c313edfdecb343b0e
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-27 14:43:06 -0700

    printf project string file

---
+++ printf_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * printStr - use variadic functin to access string parameter in printf
+ * @args: variadic variable that holds printf string parameter
+ * Return: -1 on failure, number of bytes printed on success
+ */
+
+int printStr(va_list args)
+{
+	/*this variable will count bytes returned by write function*/
+	int numOfBytes;
+
+	/*variable to itterate through loop*/
+	int itterator;
+
+	/*pointer to string var that will be read from print argument list*/
+	char *strArg;
+
+	numOfBytes = 0;
+	itterator = 0;
+
+	/*use variadic variable to extract string argument from print*/
+	str = va_arg(args, char *);
+
+	/*check if va_arg returned null*/
+	if (str == NULL)
+	{
+		return (-1);
+	}
+
+	/*use while loop to itterate through str variable*/
+	while (str[i])
+	{
+		/*assign number of bytes returned by write to numOfBytes*/
+		numOfBytes = write(1, &str[i], 1);
+		i++;
+	}
+	return (numOfBytes);
+}
