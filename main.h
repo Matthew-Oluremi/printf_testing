26fe0318bb32caa185627511c1b4f5fbe9f6f171
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-27 13:09:46 -0700

    printf project main header file

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int (*check_special_char(const char *format))(va_list);
+
+int printChar(va_list);
+int printStr(va_list);
+int printCent(va_list);
+
+/**
+ * struct struct_withFunc - struct with functions to identify special chars
+ * @t: character function
+ * @f: pointer to printing function
+ */
+
+typedef struct struct_withFunc
+{
+	char *t;
+	int (*f)(va_list);
+} print_fun;
+
+#endif /*MAIN_H*/
