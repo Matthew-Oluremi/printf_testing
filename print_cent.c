0fff698db5742180301bbf5bdbb0c2cbd6482cdb
Matthew Oluremi <matthewoluremi376@gmail.com>
2023-03-27 14:49:57 -0700

    print % character

---
+++ print_cent.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * printCent - prints the character %
+ * @args: variadic variable to hold printf parameter
+ * Return: number of bytes printed
+ */
+
+int printCent(va_list args)
+{
+	int numOfBytes;/*count bytes printed*/
+
+	(void)args;
+
+/*use write function to simply print % and allocate to numOfBytes*/
+	numOfBytes = write(1, "%", 1);
+
+	return (numOfBytes);
+}
