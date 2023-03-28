b12b5a6f683db8d61348f4966aa56aea74c3f513
SlaveToPurpose <122865752+SlaveToPurpose@users.noreply.github.com>
2023-03-28 18:15:27 +0200

    Update main.h

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int _printf(const char *format, ...);
 int (*check_special_char(const char *format))(va_list);
@@ -11,8 +12,7 @@ int (*check_special_char(const char *format))(va_list);
 int printChar(va_list);
 int printStr(va_list);
 int printCent(va_list);
-int printDec(va_list args);
-int printInt(va_list args);
+
 /**
  * struct struct_withFunc - struct with functions to identify special chars
  * @t: character function
@@ -24,5 +24,8 @@ typedef struct struct_withFunc
 	char *t;
 	int (*f)(va_list);
 } print_fun;
+
+int printDec(va_list args);
+int printInt(va_list args);
 
 #endif /*MAIN_H*/
